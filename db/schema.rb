43359e7fcef24a62f665a24502fb7281939c6774
ybadmus <yusif.badmus@gmail.com>
2021-05-01 20:20:51 +0100

    merge

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_05_01_115924) do
+ActiveRecord::Schema.define(version: 2021_05_01_181703) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "categories", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.string "story_image"
+    t.string "detail_media"
+    t.string "source"
+    t.integer "posted_by"
+    t.integer "category_id"
+    t.boolean "image_detail_media"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

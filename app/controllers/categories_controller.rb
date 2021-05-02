dce2ba8b0118199bd6493e4c162b9aaf6cbb6415
ybadmus <yusif.badmus@gmail.com>
2021-05-02 17:01:14 +0100

    add categories controller generating mockup dynamically

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,2 +1,30 @@
 class CategoriesController < ApplicationController
+
+  def index
+    @categories = Category.all
+  end
+
+  def show
+    @invites = Category.where(id: params[:id])
+  end
+
+  def new
+    @category = Category.new
+  end
+
+  def create
+    @category = category_params
+
+    if @category.save
+        return redirect_to root_path, notice: 'New category successfully created.'
+    else
+      flash.now[:alert] = @category.errors.full_messages.first
+      render :new
+    end
+  end
+
+  def category_params
+    params.require(:category).permit(:name)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :categories
   resources :posts
+  resources :home, only: [:index, :show, :new, :create]
   devise_for :users
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  root to: "home#index"
+  root to: "categories#index"
 
 end

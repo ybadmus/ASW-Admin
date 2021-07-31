c4a7ba55375af742abbfdc722772ae67b3353618
ybadmus <yusif.badmus@gmail.com>
2021-07-31 00:40:02 +0000

    add new column to post table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_233045) do
+ActiveRecord::Schema.define(version: 2021_07_31_003050) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,7 @@ ActiveRecord::Schema.define(version: 2021_07_21_233045) do
     t.boolean "image_detail_media"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.boolean "trending", default: false
   end
 
   create_table "users", force: :cascade do |t|

4e601d1d6f41196d3c4c2be6f0a08209ba5088a7
ybadmus <yusif.badmus@gmail.com>
2021-08-13 15:35:12 +0000

    allowed cors for all endpoints

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
     origins 'http://localhost:3000'
 
-    resource '/api/v1/*',
+    resource '/*',
       headers: :any,
       methods: [:get, :post, :options, :patch, :delete], expose: ['X-Total-Count', 'X-Load-More', 'Link']
   end

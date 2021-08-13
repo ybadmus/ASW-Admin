1ff6999b19c842d4a74b054f27ce4f337e6f6571
Yusif Badmus <yusif.badmus@gmail.com>
2021-08-13 15:22:12 +0000

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://61168d3e4c67c20937dbb508--mystifying-ride-487431.netlify.app'
+    origins 'https://mystifying-ride-487431.netlify.app/'
 
     resource '/api/v1/*',
       headers: :any,

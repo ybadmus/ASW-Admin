b4b776a7393bd0a2f013cfc7cb662dfb7545770c
Yusif Badmus <yusif.badmus@gmail.com>
2021-08-13 15:38:11 +0000

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,8 +1,8 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mystifying-ride-487431.netlify.app/'
+    origins 'https://mystifying-ride-487431.netlify.app'
 
-    resource '/api/v1/*',
+    resource '/*',
       headers: :any,
       methods: [:get, :post, :options, :patch, :delete], expose: ['X-Total-Count', 'X-Load-More', 'Link']
   end

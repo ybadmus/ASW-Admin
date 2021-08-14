542b73e3807aedd27619cb1678bb282db710c6d8
Yusif Badmus <yusif.badmus@gmail.com>
2021-08-14 19:26:36 +0000

    Update cors.rb

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://awutusenyawatch.com'
+    origins 'https://awutusenyawatch.com','https://www.awutusenyawatch.com'
 
     resource '/*',
       headers: :any,

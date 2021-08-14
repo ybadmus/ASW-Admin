073fa541290f380278d0c5e677806968bd259108
ybadmus <yusif.badmus@gmail.com>
2021-08-14 15:48:06 +0000

    update gmail settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -119,4 +119,18 @@ Rails.application.configure do
   # config.active_record.database_selector = { delay: 2.seconds }
   # config.active_record.database_resolver = ActiveRecord::Middleware::DatabaseSelector::Resolver
   # config.active_record.database_resolver_context = ActiveRecord::Middleware::DatabaseSelector::Resolver::Session
+
+  config.action_mailer.default_url_options = { host: 'awutusenyawatch.com' }
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.perform_deliveries = true 
+  ActionMailer::Base.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+    :address              => "smtp.gmail.com",
+    :port                 => 587,
+    :domain               => "gmail.com",
+    :authentication       => "plain",
+    :user_name            => "yusif.badmus@gmail.com",
+    :password             => "uuenoapebudnapva",
+    :enable_starttls_auto => true
+  }
 end

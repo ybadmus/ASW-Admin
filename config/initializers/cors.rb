b9ad12b3c9b8d6dcace1b252aef880cfa3485ae0
ybadmus <yusif.badmus@gmail.com>
2021-11-04 10:09:30 +0000

    add awutu senya watch domain to api

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -2,7 +2,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://mystifying-ride-487431.netlify.app', 'http://localhost:3000'
+    origins 'https://mystifying-ride-487431.netlify.app', 'https://awutusenyawatch.com', 'http://localhost:3000'
 
 
     resource '/*',
